48c1d69ea740e3df22ad6bc85cd4332095e2fd49
tahmeedhasan88 <hasantahmeed416@gmail.com>
2025-12-23 21:35:24 +0600

    fixing running

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
-import { RouterProvider } from "react-router/dom";
 import { router } from './Routes/Router.jsx';
 import AuthProvider from './Context/AuthProvider.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RouterProvider } from 'react-router';
 
 
 const queryClient = new QueryClient()
