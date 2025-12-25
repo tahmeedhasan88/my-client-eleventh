fe666d5d4e80ead953b231a241fa94daf34d7e67
tahmeedhasan88 <hasantahmeed416@gmail.com>
2025-12-25 21:02:42 +0600

    fixing error

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { router } from './Routes/Router.jsx';
 import AuthProvider from './Context/AuthProvider.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
